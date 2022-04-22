6d997815fae84f5104b868a260da529d3ad796d1
TheMrPixelDev <julian.harrer@posteo.de>
2022-04-22 14:05:57 +0200

    fixed service worker

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,14 @@
+var urlsToCache = [
+    '/style.css',
+    '/shake.js',
+    '/app.js',
+    '/index.html'
+]
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('soundboard-static-v1').then(function (cache) {
-            return cache.addAll([
-                '/style.css',
-                '/shake.js',
-                '/app.js',
-                '/index.html'
-            ])
+            return cache.addAll(urlsToCache);
         })
     );
     self.skipWaiting();
@@ -17,4 +19,28 @@ self.addEventListener("activate", (event) => {
         if ("navigationPreload" in self.registration) await self.registration.navigationPreload.enable();
     })());
     self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        (async () => {
+            try {
+                const preloadResponse = await event.preloadResponse;
+                if (preloadResponse) return preloadResponse;
+            } catch (error) {
+            }
+            return fetch(event.request).then(function (response) {
+                if (!response || response.status !== 200 || response.type !== 'basic')
+                    return response;
+                let responseToCache = response.clone();
+                caches.open(urlsToCache.includes(event.request.url.replaceAll(/.*:\/\/?([^\/]+)/g, "")) ? "soundboard-static-v1" : "other")
+                    .then(function (cache) {
+                        cache.put(event.request, responseToCache);
+                    });
+                return response;
+            }).catch(function () {
+                return caches.match(event.request);
+            });
+        })()
+    );
 });
